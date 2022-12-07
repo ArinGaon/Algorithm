6362e31181cd758fed809df3980d63da5483b571
ArinGaon <85141162+ArinGaon@users.noreply.github.com>
2022-12-07 15:30:55 +0900

    [Silver V] Title: 색종이, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.cc
+++ 백준/Silver/2563. 색종이/색종이.cc
@@ -1,62 +1,29 @@
 #include <iostream>
-
-
 using namespace std;
+// 도화지 크기가 100이므로 0~100 전역변수 101 및 이차원 배열 선언
+const int SIZE = 100+1;
+int arr[SIZE][SIZE];
 
- 
-
-const int MAX = 100 + 1;
-
-
-
-int coord[MAX][MAX];
-
- 
-
-int main(void)
-
-{
-
-        int N;
-
-        cin >> N;
-
- 
-
-        for (int k = 0; k < N; k++)
-
-        {
-
-                 int x, y;
-
-                 cin >> x >> y;
-
- 
-
-                 for (int i = y + 1; i <= y + 10; i++)
-
-                         for (int j = x + 1; j <= x + 10; j++)
-
-                                 coord[i][j] = 1;
-
-        }
-
- 
-
-        int result = 0;
-
-        for (int i = 1; i < MAX; i++)
-
-                 for (int j = 1; j < MAX; j++)
-
-                         if (coord[i][j])
-
-                                 result++;
-
- 
-
-        cout << result << endl;
-
-        return 0;
-
+int main() {
+	int num;
+	cin >> num;
+	// 색종이를 붙이는 부분을 1로 정함
+	for (int p = 0; p < num; p++) {
+		int a, b;
+		cin >> a >> b;
+		for (int i = b+1; i <= b + 10; i++) {
+			for (int j = a+1; j <= a + 10; j++) {
+				arr[i][j] = 1;
+			}
+		}				
+	}
+	// 1의 갯수가 곧 색종이를 붙인 부분의 넓이이므로 1의 갯수를 센다
+	int cnt = 0;
+	for (int i = 1; i < SIZE; i++) 
+		for (int j = 1; j < SIZE; j++) 
+			if (arr[i][j])
+				cnt++;
+		
+	cout << cnt << endl;
+	return 0;
 }
