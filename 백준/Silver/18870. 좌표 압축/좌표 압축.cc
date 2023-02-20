cf3671af2e9592637ed60904fd52c1e3a2fe3bc0
ArinGaon <85141162+ArinGaon@users.noreply.github.com>
2023-02-20 11:21:18 +0900

    [Silver II] Title: 좌표 압축, Time: 984 ms, Memory: 9840 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> v(n); 
+
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+
+    vector<int> cv(v);
+    sort(cv.begin(), cv.end());
+
+    cv.erase(unique(cv.begin(), cv.end()), cv.end());
+    for (int i = 0; i < n; i++) {
+        auto it = lower_bound(cv.begin(), cv.end(), v[i]);
+        cout << it - cv.begin() << ' ';
+    }
+
+    return 0;
+}
