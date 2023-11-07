d492e85e055f8f62bea837af3b06b23f962f00ea
ArinGaon <85141162+ArinGaon@users.noreply.github.com>
2023-11-07 12:08:38 +0900

    Update 듣보잡.cc

--- 백준/Silver/1764. 듣보잡/듣보잡.cc
+++ 백준/Silver/1764. 듣보잡/듣보잡.cc
@@ -1,28 +1,58 @@
 #include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
 #include <map>
+#include <vector>
+#include <algorithm>
+#include <string>
+
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-    map<string, int> ma;
-    vector<string> vt;
-    int n, m;
-    cin >> n >> m;
-    for (int i = 0; i < n+m; i++) {
-        string str;
-        cin >> str;
-        ma[str]++;
-        if (ma[str] > 1)
-            vt.push_back(str);
-    }
-    sort(vt.begin(), vt.end());
-    cout << vt.size() << '\n';
-    for (auto o : vt)
-        cout << o << '\n';
 
+	int n, m;
+	cin >> n >> m;
+	map<string, int> map;
+	vector<string> v;
+	
+	for (int i = 0; i < n + m; i++) {
+		string name;
+		cin >> name;
+		map[name]++;
+		if (map[name] > 1)
+			v.push_back(name);
+	}
+	sort(v.begin(), v.end());
+	cout << v.size() << "\n";
+	for (auto p : v) // 범위기반 for문 
+		cout << p << "\n";
+
+	return 0;
 }
+
+/* 우씨 이렇게 했더니 또 시간초과 뜸
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n, m;
+	cin >> n >> m;
+	unordered_map<int, string> no_H; // no Heard
+	unordered_map<int, string> no_L; // no Look........
+	string name;
+	for (int i = 0; i < n; i++) {
+		cin >> name;
+		no_H.insert(make_pair(i, name));
+	}
+	for (int i = 0; i < m; i++) {
+		cin >> name;
+		no_L.insert(make_pair(i, name));
+	}
+	for (int i = 0; i < n; i++) {
+		auto it = no_H.find(i);
+		for (int j = 0; j < m; j++) {
+			auto im = no_L.find(j);
+			if (it->second == im->second)
+				cout << it->second << "\n";
+		}
+	}
+}
+*/
